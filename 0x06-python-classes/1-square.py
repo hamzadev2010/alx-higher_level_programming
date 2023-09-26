cc0e115714d37e9cb01f890e766c557b24c3460b
hamza <hamzachanik@outlook.com>
2023-09-26 06:38:50 -0700

    task1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+empty
+"""
+
+
+class Square:
+    """
+    intruduce a square with instance size,
+    instantiation with size.
+    """
+    def __init__(self, size):
+        """
+        size : no verification for type or value
+        ----------------------------
+        Private instance attribute size
+        """
+        self.__size = size
