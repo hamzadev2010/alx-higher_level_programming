63fa017f83f10e4fb1e1cb555080c5f08eeb9576
hamza <hamzachanik@outlook.com>
2023-10-10 07:03:38 -0700

    task

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+
 """check if object if instance of a class that"""
+
 def inherits_from(obj, a_class):
     """true object is an instance of the class that
     inherited """
+
     if issubclass(obj.__class__, a_class) is True:
         if obj.__class__ is not a_class:
             return True
