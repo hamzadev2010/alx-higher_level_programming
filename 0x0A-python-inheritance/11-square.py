cafbbb2300957ecf21805702ab684575559eab44
hamza <hamzachanik@outlook.com>
2023-10-10 09:11:01 -0700

    task

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Creates a Square subclass of Rectangle."""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Intoduce a square."""
+
+    def __init__(self, size):
+        """Initialize the new square.
+            size int: The size of the new square.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
