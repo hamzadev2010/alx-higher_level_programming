45c5a0fd4f16ec42e43a2b797c77d3e406e08aca
hamza <hamzachanik@outlook.com>
2023-10-10 09:55:38 -0700

    task

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""3-to_json_string.py"""
+import json
+
+
+def to_json_string(my_obj):
+    """A function that provides the JSON representation of an object as a string."""
+
+    return json.dumps(my_obj)
