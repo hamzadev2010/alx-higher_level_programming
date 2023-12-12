95fefa5177b0820182e0a96b3f639917eac32980
hamza <hamzachanik@outlook.com>
2023-12-12 06:11:46 -0800

    task

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 // Display and sorted the biggest number
-if (process.argv.length < 4) {
-  console.log('0');
+const num = process.argv;
+if (num.length <= 3) {
+  console.log(0);
 } else {
-  const num = process.argv.slice(2).map(Number);
-  const numsort = num.sort((a, b) => b - a);
-  console.log(num.length >= 3 ? num[2] : '0');
+  console.log(num.sort((a, b) => b - a).slice(3)[0]);
 }
