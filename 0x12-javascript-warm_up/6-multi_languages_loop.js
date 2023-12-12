5eec23a40d759c4925717e4b2a1f6897294c078a
hamza <hamzachanik@outlook.com>
2023-12-12 05:25:17 -0800

    task

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // Display an sentence
-
 const wordsent = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 for (let i = 0; i < wordsent.length; i++) {
   console.log(wordssent[i]);
+	
 }
