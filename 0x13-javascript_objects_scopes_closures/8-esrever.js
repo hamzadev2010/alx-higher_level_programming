773308e1356e7c1c11f8dbd755cf11ae333c4f04
hamza <hamzachanik@outlook.com>
2023-12-13 05:30:33 -0800

    task

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// returns the reversed version of a list.
+exports.esrever = function (list) {
+  const lis = [];
+  for (const it of list) {
+    lis.push(it);
+  }
+  return lis;
+};
