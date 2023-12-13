1fba04b4c808afb585af17d4c6bc6553993b0303
hamza <hamzachanik@outlook.com>
2023-12-13 05:16:55 -0800

    task

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,7 +2,7 @@
 // Square of 5-square.js
 const Sqr = require('./5-square');
 module.exports = class Square extends Sqr {
-	 charPrint (c) {
+ charPrint (c) {
     for (let i = 0; i < this.width; i++) {
       if (c === undefined) {
         console.log('X'.repeat(this.height));
