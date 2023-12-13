f0111dccd68b3ff46c9476a5058a5825bb4f1e48
hamza <hamzachanik@outlook.com>
2023-12-13 06:14:48 -0800

    task

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -2,7 +2,7 @@
 // dictionary of occurrences by user id
 // and computes a dictionary of user ids by occurrence.
 const dict = require('./101-data').dict;
-Object.keys(dict).map(function (n, index) { {
+console.log(Object.keys(dict).map(function (n, index) { {
   n[index[1]] = (n[index[1]] || []).concat(index[0]);
   return n;
 }, {}));
