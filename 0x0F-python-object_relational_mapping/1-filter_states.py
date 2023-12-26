a732e6730457a7a9c1a80b105c3fbc57db42e6fe
hamza <hamzachanik@outlook.com>
2023-12-26 08:25:49 -0800

    task

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""states with a name starting with N (upper N) from the database """
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    cn = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
+     cr = conn.cursor()
+     cr.execute("SELECT * FROM states\
+                    WHERE name LIKE BINARY 'N%'\
+                    ORDER BY id ASC")
+
+    query_rows = cr.fetchall()
+
+    for row in data:
+        print(row)
+
+    cr.close()
+    cn.close()
