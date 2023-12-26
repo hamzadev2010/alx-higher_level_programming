8739e86de0564d33bc02ca2a7db5568ffaa14366
hamza <hamzachanik@outlook.com>
2023-12-26 10:06:17 -0800

    task

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
                          db=sys.argv[3], port=3306)
      cr = db.cursor()
-    cr.execute("FROM states WHERE CONVERT(`name` USING Latin1) COLLATE Latin1_General_CS 
+    cr.execute("FROM states / WHERE CONVERT(`name` USING Latin1) / COLLATE Latin1_General_CS /
     LIKE 'N%';")
      states = cr.fetchall()
 
