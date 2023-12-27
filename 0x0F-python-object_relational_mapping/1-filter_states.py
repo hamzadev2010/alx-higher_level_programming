ffe8f78c69de050bfbf5a4f4a29ec4dd3a2b0c4d
hamza <hamzachanik@outlook.com>
2023-12-27 02:05:48 -0800

    task

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """ Script that print all states"""
 
+from sys import argv
 import MySQLdb
-import sys
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
@@ -14,4 +14,4 @@ if __name__ == "__main__":
         if row[1].startswith("N"):
             print(row)
     cr.close()
-    db.close()    
+    db.close()
