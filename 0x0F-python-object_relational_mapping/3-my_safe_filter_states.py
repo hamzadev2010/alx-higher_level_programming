f4dd968bc3ce71b5e8b3be0005bda35ef95f0dc5
hamza <hamzachanik@outlook.com>
2023-12-27 03:05:58 -0800

    task

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""List of spesific states on database table"""
+
+from sys import argv
+import MySQLdb
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2],
+                         db=argv[3], port=3306)
+
+    cr = db.cursor()
+    cr.execute("SELECT * FROM states WHERE name = %s;", (i.argv[4],))
+    st = cr.fetchall()
+
+    for states in st:
+        print(states)
