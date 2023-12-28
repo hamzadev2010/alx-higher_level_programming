8a1ab47b5ef4031fc54bc4e6f3528cd4771b9979
hamza <hamzachanik@outlook.com>
2023-12-28 09:39:17 -0800

    task

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -11,9 +11,8 @@ if __name__ == '__main__':
                            format(argv[1], argv[2], argv[3]),
                            pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
+    Base.metadata.create_all(engine)
     session = Session()
     state = session.query(State).order_by(State.id).first()
-     if states is not None:
-        print("{}: {}".format(states.id, states.name))
-    else:
-        print ("Nothing")
+     if states is None: print ("Nothing")
+    else: print("{}: {}".format(states.id, states.name))
