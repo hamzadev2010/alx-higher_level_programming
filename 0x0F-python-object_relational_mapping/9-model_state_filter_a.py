b01c007e86ed21d8b38a9b7e4b4129c5c34921cb
hamza <hamzachanik@outlook.com>
2023-12-29 07:32:40 -0800

    tas

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
+""" lists all State objects that contain the letter a 
 """
-script that lists all State objects that contain the letter a from the database
-"""
-from sys import sys
+import sys
 from model_state import Base, State
-from sqlalchemy import create_engine
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
-
-if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                           format(argv[1], argv[2], argv[3]),
-                           pool_pre_ping=True)
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(sys.argv[1], sys.argv[2],
+                sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    states= session.query(State).filter(State.name.like('%a%')).all()
-
-       for state in states:
+    for state in session.query(State).\
+            filter(State.name.like('%a%')).order_by(State.id).all():
         print("{}: {}".format(state.id, state.name))
