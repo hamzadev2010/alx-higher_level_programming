2913081d659ab25c0991f7abe04e7c881e4c97b2
hamza <hamzachanik@outlook.com>
2024-01-05 07:48:49 -0800

    task

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# script that takes in a URL and displays all HTTP methods the server will accept.
+#cript that sends a DELETE request to the URL passed as the first argument and displays the body of the response
 curl -sX DELETE "$1"
