a2fc3c034f03480a690e56489437c04c8f13f2bb
hamza <hamzachanik@outlook.com>
2024-01-07 15:36:33 -0800

    task

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -8,6 +8,7 @@ from requests.auth import HTTPBasicAuth
 
 
 if __name__ == "__main__":
+
       webreq = requests.get("https://api.github.com/user", auth=auth)
-       user = HTTPBasicAuth(argv[1], argv[2])
+       auth = HTTPBasicAuth(argv[1], argv[2])
        print(webreq.json().get("id"))
