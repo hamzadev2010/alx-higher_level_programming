a8c28c6069025504f0ac61607ec0743072f6abc1
hamza <hamzachanik@outlook.com>
2024-01-07 13:01:03 -0800

    task

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+sends a POST request to the passed URL with the email as a parameter,
+and displays the body of the response 
+"""
+import urllib.request
+import urllib.parse
+from sys import argv
+
+if __name__ == "__main__":
+
+    web = argv[1]
+    val = {'email': argv[2]}
+
+    db = urllib.parse.urlencode(val).encode()
+    rq = urllib.request.Request(web, db)
+    with urllib.request.urlopen(rq) as response:
+        value = response.read().decode('utf8')
+    print(value)
