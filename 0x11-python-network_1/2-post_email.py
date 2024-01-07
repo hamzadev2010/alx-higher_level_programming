bfe253839dec9510cbecc1875a4c6e6f03df88de
hamza <hamzachanik@outlook.com>
2024-01-07 13:02:43 -0800

    task

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 sends a POST request to the passed URL with the email as a parameter,
-and displays the body of the response 
+and displays the body of the response
 """
 import urllib.request
 import urllib.parse
