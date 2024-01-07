16ee52b5bd0d8c86cd0832e8cd52db5e31bc9cdf
hamza <hamzachanik@outlook.com>
2024-01-07 13:15:28 -0800

    task

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" script that fetches and send response"""
+import requests
+
+
+if __name__ == "__main__":
+    web = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(web.text)))
+    print("\t- content: {}".format(web.text))
