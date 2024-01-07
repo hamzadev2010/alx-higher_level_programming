c6a346320b4c6e2dc6e38002fec4d64be93341e8
hamza <hamzachanik@outlook.com>
2024-01-07 12:15:05 -0800

    task

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 """script that fetches https://alx-intranet.hbtn.io/status
 """
-import urllib.request
+from urllib import request
 if __name__ == "__main__":
-    try:
        with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
         body = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
         print("\t- utf8 content: {}".format(body.decode("utf-8"))
-                   except error.URLError
