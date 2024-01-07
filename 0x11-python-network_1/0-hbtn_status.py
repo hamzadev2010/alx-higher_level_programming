c6502c3e80b8748873986b8cc5caaaf9132be826
hamza <hamzachanik@outlook.com>
2024-01-07 12:08:24 -0800

    task

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 """script that fetches https://alx-intranet.hbtn.io/status
 """
-  import urllib.request
-
+import urllib.request
 if __name__ == "__main__":
-       weburl = "https://alx-intranet.hbtn.io/status"
-    with urllib.request.urlopen(weburl) as response:
-          body = response.read()
+       with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+        body = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
