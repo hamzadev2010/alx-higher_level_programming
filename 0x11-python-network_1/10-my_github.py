99a984af7cf40a952a9a408f8c87f67b7293c3b8
hamza <hamzachanik@outlook.com>
2024-01-07 15:50:55 -0800

    task

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-"""script that takes your GitHub credentials
-(username and password) and uses the GitHub API
+"""Script that check the api with username and password
 """
-import requests
 from sys import argv
+import requests
+from requests.auth import HTTPBasicAuth
 
 
 if __name__ == "__main__":
-
-webreq = requests.get("https://api.github.com/user", auth=(argv[1], argv[2]))
-    print(webreq.json().get("id"))
+    web = "https://api.github.com/user"
+    user = argv[1]
+    pswd = argv[2]
+    res = requests.get(web, auth=HTTPBasicAuth(user, pswd))
+    js = res.json()
+    print(js.get("id"))
