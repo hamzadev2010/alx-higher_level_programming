299fa48a73eaaf313a372c335d1016ff7f61c0c9
hamza <hamzachanik@outlook.com>
2024-01-07 13:52:43 -0800

    task

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -8,7 +8,7 @@ import requests
 
 if __name__ == "__main__":
     web = "http://0.0.0.0:5000/search_user"
-    if len(sys.argv) > 1:
+    if len(argv) > 1:
         q = argv[1]
     else:
         q = ""
