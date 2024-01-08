ccd92aea9481aa6422f71527ab77de0f5f321b46
hamza <hamzachanik@outlook.com>
2024-01-07 16:39:31 -0800

    task

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -3,11 +3,9 @@
 sends a request to the URL
 and displays the value of the variable
 """
-import requests
-from sys import argv
-
-
 if __name__ == '__main__':
+    from sys import argv
+    import requests
     web = argv[1]
     webrq = requests.get(web)
     hd= webrq.headers.get('X-Request-Id')
