bd44344a83a7ab6a7a5c0f6adaeaf8f4a9217b47
hamza <hamzachanik@outlook.com>
2024-01-07 16:13:54 -0800

    task

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,11 +7,11 @@ import requests
 
 
 if __name__ == "__main__":
+
 web = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
  
  req = requests.get(web)
   commits = req.json()
   for i in range(10):
-            print("{}: {}".format(
-                commits[i].get("sha"),
-                commits[i].get("commit").get("author").get("name")))
+print("{}: {}".format(commits[i].get("sha"),
+    commits[i].get("commit").get("author").get("name")))
