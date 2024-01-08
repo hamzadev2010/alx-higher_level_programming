c12e54e563bb2704885af46a15ec879693945164
hamza <hamzachanik@outlook.com>
2024-01-07 16:09:42 -0800

    task

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -2,12 +2,15 @@
 """ Script that f evaluates candidates applying for a back-end position
 by taking 2 arg repository name and owner name
 """
-if __name__ == "__main__":
 from sys import argv
 import requests
-web = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
-req = requests.get(web)
-commits = req.json()
+
+
+if __name__ == "__main__":
+
+  web = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
+  req = requests.get(web)
+  commits = req.json()
   for i in range(10):
             print("{}: {}".format(
                 commits[i].get("sha"),
