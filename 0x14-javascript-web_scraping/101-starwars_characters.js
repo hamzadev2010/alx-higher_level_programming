e4d2076de117858e715e60348441e00690afa77c
hamza chankour <hamzachanik@outlook.com>
2024-01-30 03:58:05 -0800

    task

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+//a script that prints all characters of a Star Wars movie:
+const rq = require('request');
+rq.get('https://swapi-api.hbtn.io/api/films/' + process.argv[2] + '/', function (err, response, body) {
+  if (err) {
+    console.log(err);
+  }
+  const chac = JSON.parse(body).characters;
+  for (const characterUrl of chac) {
+    await new Promise((res, rej) => {
+      rq(characterUrl, (err, response, body) => {
+        if (err) {
+          rej(err);
+        } else {
+          console.log(JSON.parse(body).name);
+          res();
+        }
+      });
+    });
+  }
+});
