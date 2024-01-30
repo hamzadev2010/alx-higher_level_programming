2aa636c229b735be88e8f673093b10994687b6a6
hamza chankour <hamzachanik@outlook.com>
2024-01-30 02:52:41 -0800

    task

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
 //a script that writes a string to a file
+
 const fs = require('fs');
 const cn = process.argv[3];
+const cn2 = process.argv[2];
 
-fs.writeFile(process.argv[2], cn, function (err) {
+fs.writeFile(cn2, cn, 'utf8', function (err) {
   if (err) {
     console.log(err);
   }
