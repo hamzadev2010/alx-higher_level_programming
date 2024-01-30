57ee2609df8246186541b5d461f45cf8ca3b4408
hamza chankour <hamzachanik@outlook.com>
2024-01-30 03:24:48 -0800

    task

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+// script that displays the number of movies 
+const rq = require('request');
+const weburl = process.argv[2];
+
+rq(weburl, function (err, response, body) {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  let cnt = 0;
+  for (const film of JSON.parse(body).results) {
+    for (const character of film.characters) {
+      if (character.includes('18')) {
+        cnt = cnt + 1;
+      }
+    }
+  }
+  console.log(cnt);
+});
