fe6fd8eae50edc602b9439fb13b684fd22df24c6
hamza chankour <hamzachanik@outlook.com>
2024-01-30 04:03:55 -0800

    task

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 //a script that prints all characters of a Star Wars movie:
 const rq = require('request');
-rq.get('https://swapi-api.alx-tools.com/api/' + process.argv[2] + '/', function (err, response, body) {
+rq.get('https://swapi-api.alx-tools.com/api/films/' + process.argv[2] + '/', function (err, response, body) {
   if (err) {
     console.log(err);
   }
